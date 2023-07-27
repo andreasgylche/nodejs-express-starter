c4acca27f84a071f501ba549e54eb0537782ba25
Andreas Vifert <andreasgylche@gmail.com>
2023-07-27 13:30:19 +0200

    rate limiter added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 import errorhandler from 'errorhandler'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
@@ -12,6 +13,7 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
 app.use(helmet())
 app.use(cors())
 app.use(morgan('dev'))
